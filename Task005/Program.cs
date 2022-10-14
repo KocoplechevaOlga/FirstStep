4c80d22cea9dcc40b8128c14261da94a351a20fd
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-14 17:09:25 +0300

    Add Task5

---
+++ Task005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача с гирьками
+int a = 6;
+int b = 3;
+int c = 9;
+int d = 4;
+int e = 2;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("Max= ");
+Console.WriteLine(max);
