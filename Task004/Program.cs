f0ed3442cfa930ecd32bbde75d793b5142691632
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-14 16:56:21 +0300

    Add Task4

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Учимсыя использовать ветвление
+Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "оля")
+{
+Console.WriteLine("Оля-ля, да это же ОЛЯ!");
+}
+else
+{
+Console.Write("Здравствуй, ");
+Console.Write(username);
+}
