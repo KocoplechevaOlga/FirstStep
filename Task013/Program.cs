d752fcb8012af3c61418e48c86d072762e882805
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-21 22:31:26 +0300

    First Task with recurs Done

---
+++ Task013/Program.cs
@@ -0,0 +1,17 @@
+﻿// Составление слов из буквенного массива
+
+int n = 1;
+void FindWorld(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWorld(alphabet, word, length +1);
+    }
+}
+
+FindWorld("асив", new char[2]);
