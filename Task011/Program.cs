f4f3c559c880ef6f3d16906085ae26b4406761ec
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-21 20:42:36 +0300

    First Task with recurs

---
+++ Task011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Сумма чисел с рекурсией
+
+int SumRec (int n)
+{
+    if (n == 1) return 1;
+    else return n + SumRec(n-1);
+}
+
+Console.WriteLine("Введите целое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = SumRec(num);
+Console.WriteLine(res);
